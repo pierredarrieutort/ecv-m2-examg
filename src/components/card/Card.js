571229fc41cdf0d857384fe1596b8997d86d0cd6
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-12-18 11:42:44 +0100

    Fix api error

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -9,7 +9,7 @@ export function Card({ picture }) {
     const { state, dispatch } = useContext(ApplicationContext);
 
     const onLike = (pictureId) => {
-        if (picture.likedBy.find(like => like === state.user._id))
+        if (picture.likedBy.find(({_id}) => _id === state.user._id))
             unLikePictureById(dispatch, pictureId)
         else
             LikePictureById(dispatch, pictureId)
@@ -20,7 +20,7 @@ export function Card({ picture }) {
         <div className="card">
             <div className="card-img">
                 <img src={picture.download_url} />
-                <LikeButton onClick={() => { onLike(picture.id) }} isLiked={picture.likedBy && picture.likedBy.find(like => like === state.user._id)} />
+                <LikeButton onClick={() => { onLike(picture.id) }} isLiked={picture.likedBy && picture.likedBy.find(({_id}) => _id  === state.user._id)} />
                 <span className="likes">Likes : {picture.likedBy?.length ?? 0}</span>
                 <BookmarkButton onClick={() => { }} />
             </div>
